77aaf7c316aee8d577142c834ac111671ce6a0bb
carolineglass <carolineglass89@gmail.com>
2021-03-22 15:22:45 -0400

    mongoose validation and errors

--- models/User.js
+++ models/User.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+
 
 //defining user document in db
 const userSchema = new mongoose.Schema({
     email: {
         type: String, 
-        required: true,
+        required: [true, 'please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
     }, 
     password: {
         type: String,
-        required: true,
-        minlength: 6
+        required: [true, 'please enter a password'],
+        minlength: [6, 'Minimum password length is 6 characters']
     }
 });
 
