e5a735585fa1317a393760c1b86985f97c1489cb
carolineglass <carolineglass89@gmail.com>
2021-03-23 14:15:05 -0400

    checkUser middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
@@ -6,10 +7,8 @@ const requireAuth = (req, res, next) => {
     if (token) {
         jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
             if (err) {
-                console.log(err.message);
                 res.redirect('/login');
             } else {
-                console.log(decodedToken);
                 next();
             }
         })
@@ -18,4 +17,26 @@ const requireAuth = (req, res, next) => {
     }
 }
 
-module.exports = { requireAuth };
+//check current user 
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message)
+                res.locals.user = null;
+                next();
+            } else {
+                let user = await User.findById(decodedToken.id);
+                //makes the user accessible by the views
+                res.locals.user = user;
+                next();
+            }
+        })
+    } else {
+        res.locals.user = null;
+        next();
+    };
+};
+
+module.exports = { requireAuth, checkUser };
